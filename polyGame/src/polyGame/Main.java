9fcc20af98c81dcbcf3e8b128142b402a47d112d
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-01 17:11:51 +0900

    Modify : Main class

--- polyGame/src/polyGame/Main.java
+++ polyGame/src/polyGame/Main.java
@@ -1,10 +1,14 @@
 package polyGame;
 
 public class Main {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		GameManager gameManager = new GameManager();
+		boolean run = true;
+		while(true) {
+			run = gameManager.changeStage();
+			if(run == false) 
+				break;
+		}
+		System.out.println("GAME OVER-");
 	}
-
 }
