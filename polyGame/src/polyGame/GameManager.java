c37714fe46a87e20e972e3c5e843c102a5df8466
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-02 14:03:26 +0900

    Add : class & variable

--- polyGame/src/polyGame/GameManager.java
+++ polyGame/src/polyGame/GameManager.java
@@ -1,6 +1,18 @@
 package polyGame;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import java.util.Scanner;
+
 public class GameManager {
+	Random random = new Random();
+	static Scanner scan = new Scanner(System.in);
+	
+	static String nextStage = "";
+	String curStage = "";
+	
+	Map<String, Stage> stageList = new HashMap<String, Stage>();
 	
 	public boolean changeStage() {
 		return true;
