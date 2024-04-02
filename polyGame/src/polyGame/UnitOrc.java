83e4d8a7d1606ec81255b5a8d134ed60595f6ff4
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-02 12:27:11 +0900

    Add : constructor & skill 메소드 추가

--- polyGame/src/polyGame/UnitOrc.java
+++ polyGame/src/polyGame/UnitOrc.java
@@ -1,5 +1,12 @@
 package polyGame;
 
-public class UnitOrc {
+public class UnitOrc extends Unit {
 
+	public UnitOrc() {
+		setName("오크");
+	}
+	
+	public void skill() {
+		System.out.println("오크 스킬");
+	}
 }
