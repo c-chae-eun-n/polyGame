e540264f37298d8ea2c28beec90e4efd502fea01
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-02 12:26:53 +0900

    Add : constructor & skill 메소드 추가

--- polyGame/src/polyGame/UnitBat.java
+++ polyGame/src/polyGame/UnitBat.java
@@ -1,5 +1,12 @@
 package polyGame;
 
-public class UnitBat {
+public class UnitBat extends Unit {
 
+	public UnitBat() {
+		setName("박쥐");
+	}
+	
+	public void skill() {
+		System.out.println("박쥐 스킬");
+	}
 }
