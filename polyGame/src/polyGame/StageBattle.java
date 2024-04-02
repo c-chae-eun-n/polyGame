319091b24d456c39ad6f07475064ce9649492810
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-02 14:23:44 +0900

    Update : monsterRandomSet method

--- polyGame/src/polyGame/StageBattle.java
+++ polyGame/src/polyGame/StageBattle.java
@@ -1,6 +1,15 @@
 package polyGame;
 
+import java.util.Random;
+import java.util.Vector;
+
 public class StageBattle extends Stage {
+	UnitManager unitManager = new UnitManager();
+	Vector<Player> playerList = null;
+	Vector<Unit> monsterList = null;
+	Random random = new Random();
+	int monDead = 0;
+	int playerDead = 0;
 
 	@Override
 	public boolean update() {
@@ -9,7 +18,17 @@ public class StageBattle extends Stage {
 
 	@Override
 	public void monsterRandomSet() {
+		unitManager.monsterList.clear();
+		unitManager.monsterRandomSet(4);
 		
+		playerList = null;
+		playerList = unitManager.playerList;
+		
+		monsterList = null;
+		monsterList = unitManager.monsterList;
+		
+		monDead = monsterList.size();
+		playerDead = playerList.size();
 	}
 
 }
