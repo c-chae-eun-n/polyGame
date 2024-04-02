50136e380b2f734a077d6d0ad290caa8aa095438
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-02 18:28:48 +0900

    Style: Additions related to styling only

--- polyGame/src/polyGame/StageTitle.java
+++ polyGame/src/polyGame/StageTitle.java
@@ -5,9 +5,9 @@ public class StageTitle extends Stage {
 	@Override
 	public boolean update() {
 		String start = "";
-		System.out.println("======= TEXT RPG =======");
+		System.out.println("============= TEXT RPG =============");
 		while(!start.equals("시작")) {
-			start = GameManager.inputString("    [시작]을 입력하세요.");
+			start = GameManager.inputString("\t  [시작]을 입력하세요.");
 		}
 		GameManager.nextStage = "LOBBY";
 		return false;
