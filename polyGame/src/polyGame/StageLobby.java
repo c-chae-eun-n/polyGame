7b4e00c8b4ca441fbdc0636a23713bd9a1250b80
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-02 16:52:22 +0900

    Style: Additions related to styling only

--- polyGame/src/polyGame/StageLobby.java
+++ polyGame/src/polyGame/StageLobby.java
@@ -4,15 +4,18 @@ public class StageLobby extends Stage {
 
 	@Override
 	public boolean update() {
-		System.out.println("==== [LOBBY] ====");
-		System.out.println(" [1.전투] [2.종료]");
-		int sel = GameManager.inputNumber("");
-		if(sel == 1) {
-			GameManager.nextStage = "BATTLE";
-		} else if(sel == 2) {
-			GameManager.nextStage = "";
-		} else {
-			GameManager.nextStage = "";
+		int sel = 0;
+		while(sel != 1 && sel != 2) {
+			System.out.println("======= [LOBBY] =======");
+			System.out.print("   [1.전투]   [2.종료]");
+			sel = GameManager.inputNumber("");
+			if(sel == 1) {
+				GameManager.nextStage = "BATTLE";
+			} else if(sel == 2) {
+				GameManager.nextStage = "";
+			} else {
+				System.err.println("1 또는 2만 입력해주세요.");
+			}
 		}
 		
 		return false;
