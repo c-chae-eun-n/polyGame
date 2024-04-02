ad41425c4cb7863a53d12b4b449aceae1a8232ba
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-02 14:15:42 +0900

    Add : update method

--- polyGame/src/polyGame/StageLobby.java
+++ polyGame/src/polyGame/StageLobby.java
@@ -1,5 +1,27 @@
 package polyGame;
 
-public class StageLobby {
+public class StageLobby extends Stage {
+
+	@Override
+	public boolean update() {
+		System.out.println("==== [LOBBY] ====");
+		System.out.println(" [1.전투] [2.종료]");
+		int sel = GameManager.scan.nextInt();
+		if(sel == 1) {
+			GameManager.nextStage = "BATTLE";
+		} else if(sel == 2) {
+			GameManager.nextStage = "";
+		} else {
+			GameManager.nextStage = "";
+		}
+		
+		return false;
+	}
+
+	@Override
+	public void monsterRandomSet() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
