298715bfb71c24c13a31d8f5be42508bd5c57586
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-02 11:50:02 +0900

    Add : collection

--- polyGame/src/polyGame/UnitManager.java
+++ polyGame/src/polyGame/UnitManager.java
@@ -1,5 +1,16 @@
 package polyGame;
 
-public class UnitManager {
+import java.util.Random;
+import java.util.Vector;
 
+public class UnitManager {
+	Vector<Player> playerList = new Vector<>();
+	Vector<Unit> monsterList = new Vector<>();
+	
+	String path = "polyGame.";
+	String monsters[] = {"UnitWolf", "UnitBat", "UnitOrc"};
+	Random random = new Random();
+
+	
+	
 }
