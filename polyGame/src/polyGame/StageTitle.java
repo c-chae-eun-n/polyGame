ea4d35490bb999e30cba9241212699964beed10c
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-02 15:31:24 +0900

    Update : update method

--- polyGame/src/polyGame/StageTitle.java
+++ polyGame/src/polyGame/StageTitle.java
@@ -5,10 +5,11 @@ public class StageTitle extends Stage {
 	@Override
 	public boolean update() {
 		System.out.println("==== TEXT RPG ===");
-		System.out.println("[시작]을 입력하세요");
+		System.out.println("[시작]을 입력하세요.");
 		String start = GameManager.scan.next();
 		if(start.equals("시작"))
-			GameManager.nextStage = "LOBBY";
+			;
+		GameManager.nextStage = "LOBBY";
 		return false;
 	}
 
