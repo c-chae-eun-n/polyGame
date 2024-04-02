3dbe3774c949bc7bb32d39b3b91d336d2aacf8f3
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-02 12:27:16 +0900

    Add : constructor & skill 메소드 추가

--- polyGame/src/polyGame/UnitWolf.java
+++ polyGame/src/polyGame/UnitWolf.java
@@ -1,5 +1,12 @@
 package polyGame;
 
-public class UnitWolf {
+public class UnitWolf extends Unit {
 
+	public UnitWolf() {
+		setName("늑대");
+	}
+	
+	public void skill() {
+		System.out.println("늑대 스킬");
+	}
 }
