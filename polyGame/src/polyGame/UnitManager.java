44006f12941f8b243f4ee8b58160dc85d4df5573
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-03 17:25:15 +0900

    Add : playerRandomSet & selectPlayer method

--- polyGame/src/polyGame/UnitManager.java
+++ polyGame/src/polyGame/UnitManager.java
@@ -9,12 +9,11 @@ public class UnitManager {
 	
 	String path = "polyGame.";
 	String monsters[] = {"UnitWolf", "UnitBat", "UnitOrc"};
+	String players[] = {"Warrior", "Wizard", "Healer"};
 	Random random = new Random();
 
 	public UnitManager() {
-		playerList.add(new Player("전사", 1000, 80));
-		playerList.add(new Player("마법사", 800, 65));
-		playerList.add(new Player("힐러", 500, 45));
+		
 	}
 	
 	public void monsterRandomSet(int size) {
@@ -26,11 +25,45 @@ public class UnitManager {
 				Unit temp = (Unit) obj;
 				int hp = random.nextInt(100) + 100;
 				int power = random.nextInt(10) + 10;
-				temp.setMonster(hp, power);
+				temp.setPlay(hp, power);
 				monsterList.add(temp);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 	}
+	
+	public void playerRandomSet(String players) {
+		try {
+			int hp = random.nextInt(501) + 500;
+			int power = random.nextInt(51) + 40;
+			Class<?> clazz = Class.forName(path + players);
+			Class<?>[] params = new Class<?>[] {int.class, int.class};
+			Object obj = clazz.getDeclaredConstructor(params).newInstance(hp, power);
+			Player temp = (Player) obj;
+			playerList.add(temp);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public String selectPlayer() {
+		String player = "";
+		int index = -1;
+		for(int i=0; i<players.length; i++) {
+			System.out.println((i+1) + ") " + players[i]);
+		}
+		while(true) {
+			index = GameManager.inputNumber("원하는 플레이어 번호 입력 : ")-1;
+			if(index >= 0 && index < players.length)
+				break;
+			else
+				System.err.printf("%d~%d만 입력 가능합니다.", 1, players.length);
+		}
+		
+		player = players[index];
+		
+		return player;
+	}
+	
 }
