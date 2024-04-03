b57820eaa0663d360bd57769dbdc504f09342e5c
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-03 17:24:23 +0900

    Update : constructor

--- polyGame/src/polyGame/Healer.java
+++ polyGame/src/polyGame/Healer.java
@@ -2,8 +2,8 @@ package polyGame;
 
 public class Healer extends Player {
 
-	public Healer(String name, int max, int power) {
-		super(name, max, power);
+	public Healer(int max, int power) {
+		super("힐러", max, power);
 	}
 
 }
