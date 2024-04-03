ccff369781f816e965534b00aaca9ee034832ef7
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-03 18:13:09 +0900

    Update : skill method

--- polyGame/src/polyGame/Warrior.java
+++ polyGame/src/polyGame/Warrior.java
@@ -10,7 +10,6 @@ public class Warrior extends Player {
 	public void skill(Unit target) {
 		System.err.println("[전사] 공격력 2배!!");
 		target.setCurHp(target.getCurHp()-(getPower()*2));
-		setCurHp(getCurHp()+(getPower()*2));
 		System.err.println("[" + getName() + "]가 " + "[" + target.getName() + "]에게 " + getPower()*2 + "의 데미지를 입힙니다.");
 		if(target.getCurHp() <= 0) {
 			System.err.println("[" + target.getName() + "]을(를) 처치했습니다.\n");
