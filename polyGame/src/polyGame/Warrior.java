5a0e2a9a21ee2305f990a74ebcb8cbf0c446f662
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-03 18:11:57 +0900

    Add : skill method

--- polyGame/src/polyGame/Warrior.java
+++ polyGame/src/polyGame/Warrior.java
@@ -6,6 +6,16 @@ public class Warrior extends Player {
 		super("전사", max, power);
 	}
 
-
+	@Override
+	public void skill(Unit target) {
+		System.err.println("[전사] 공격력 2배!!");
+		target.setCurHp(target.getCurHp()-(getPower()*2));
+		setCurHp(getCurHp()+(getPower()*2));
+		System.err.println("[" + getName() + "]가 " + "[" + target.getName() + "]에게 " + getPower()*2 + "의 데미지를 입힙니다.");
+		if(target.getCurHp() <= 0) {
+			System.err.println("[" + target.getName() + "]을(를) 처치했습니다.\n");
+			target.setCurHp(0);
+		}
+	}
 
 }
