22bc7b70532d3a84b7cb2001c3cadae6376e0db2
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-04 10:06:27 +0900

    Update : skill method

--- polyGame/src/polyGame/UnitBat.java
+++ polyGame/src/polyGame/UnitBat.java
@@ -8,6 +8,10 @@ public class UnitBat extends Unit {
 	
 	public void skill(Unit target) {
 		System.err.println("[박쥐] 바람일으키기-! 생명력 흡혈!!");
+		if(target.isShield()) {
+			System.err.println("[" + target.getName() + "]" + " 쉴드 발동 공격 실패!!");
+			return;
+		}
 		target.setCurHp(target.getCurHp()-(getPower()*2));
 		setCurHp(getCurHp()+(getPower()*2));
 		System.err.println("[" + getName() + "]가 " + "[" + target.getName() + "]에게 " + getPower()*2 + "의 생명력을 흡혈합니다.");
