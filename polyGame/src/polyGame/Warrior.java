612665821320aeb958b926374d470e3f670ad4aa
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-04 10:19:15 +0900

    Update : skill method

--- polyGame/src/polyGame/Warrior.java
+++ polyGame/src/polyGame/Warrior.java
@@ -8,13 +8,13 @@ public class Warrior extends Player {
 
 	@Override
 	public void skill(Unit target) {
-		System.err.println("[전사] 공격력 2배!!");
-		target.setCurHp(target.getCurHp()-(getPower()*2));
-		System.err.println("[" + getName() + "]가 " + "[" + target.getName() + "]에게 " + getPower()*2 + "의 데미지를 입힙니다.");
+		System.err.println("[전사] 쉴드 발동!!");
+		setShield(true);
+		target.setCurHp(target.getCurHp()-(getPower()/2));
+		System.err.println("[" + target.getName() + "]가 " + "[" + getName() + "]의 쉴드 효과에 " + getPower()/2 + "의 데미지를 입었다.");
 		if(target.getCurHp() <= 0) {
-			System.err.println("[" + target.getName() + "]을(를) 처치했습니다.\n");
+			System.err.println("[" + target.getName() + "]을(를) 처치했습니다.");
 			target.setCurHp(0);
 		}
 	}
-
 }
