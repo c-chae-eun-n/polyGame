7c7b06747bdd19e259a8e89bdbde5a1867a8ef0e
c-chae-eun-n <ce.hue125@gmail.com>
2024-04-04 11:11:49 +0900

    Update : skill method

--- polyGame/src/polyGame/UnitOrc.java
+++ polyGame/src/polyGame/UnitOrc.java
@@ -8,16 +8,6 @@ public class UnitOrc extends Unit {
 	
 	public void skill(Unit target) {
 		System.err.println("[오크] 격노-! 3배 공격력으로 강타!!");
-		if(isStun()) {
-			System.err.println("스턴이 걸려서 공격 실패!!");
-			setStun(false);
-			return;
-		}
-		if(target.isShield()) {
-			System.err.println("[" + target.getName() + "]" + " 쉴드 발동 공격 실패!!");
-			target.setShield(false);
-			return;
-		}
 		target.setCurHp(target.getCurHp()-(super.getPower()*3));
 		System.err.println("[" + super.getName() + "]가 " + "[" + target.getName() + "]에게 " + super.getPower()*3 + "의 데미지를 입힙니다.");
 		if(target.getCurHp() <= 0) {
